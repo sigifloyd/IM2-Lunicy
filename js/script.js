9eaeaa7d16c606bc1e98ca50a8633db1dd981316
sigifloyd <cyrill.felder@stud.fhgr.ch>
2024-05-06 17:54:58 +0200

    Willkommen Betrüger

--- js/script.js
+++ js/script.js
@@ -15,18 +15,49 @@ function fetchAge() {
         .then(data => {
             // Das Element für das Anzeigeergebnis wird abgerufen.
             const resultDiv = document.getElementById('result');
-
+        
             // Überprüfung, ob ein Alter in den Daten vorhanden ist, und Anzeige des Alters.
             if (data.age) {
-                resultDiv.innerHTML = `Dein Opfer ist mit hoher warscheinlichkeit ${data.age} Jahre alt.`;
+                // Verwende <span> tags, um das Alter in einer anderen Schriftart und Größe zu formatieren
+                resultDiv.innerHTML = `Dein Opfer ist mit hoher Wahrscheinlichkeit <span class="age">${data.age}</span> Jahre alt.`;
             } else {
                 // Falls keine Daten gefunden wurden, wird eine entsprechende Nachricht angezeigt.
                 resultDiv.innerHTML = "Keine Daten gefunden. Bitte überprüfen Sie die Eingaben.";
             }
         })
+        
         .catch(error => {
             // Bei einem Fehler in der Anfrage wird dieser im Konsolen-Log ausgegeben und eine Fehlermeldung angezeigt.
             console.error('Fehler beim Abrufen der Daten: ', error);
             document.getElementById('result').innerHTML = "Fehler bei der Anfrage.";
         });
+}
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
 }

42fc2ec6056e6b7e1568c32b1b5930f6f06c2596
Nick Steinmann <nick.steinmann@stud.fhgr.ch>
2024-05-07 15:07:06 +0200

    Aktueller Stand, Improved Slider, Tablet und Desktop optimierung

--- js/script.js
+++ js/script.js
@@ -1,42 +1,39 @@
-// js/script.js
-
-// Funktion `fetchAge` definiert, die aufgerufen wird, wenn der Benutzer auf den Button klickt.
 function fetchAge() {
-    // Die Werte der Input-Felder für Name und Land werden abgerufen und in Variablen gespeichert.
     const name = document.getElementById('name').value;
     const country = document.getElementById('country').value;
-
-    // Erstellung der URL für die API-Anfrage, die den Namen und den Ländercode enthält.
     const url = `https://api.agify.io?name=${name}&country_id=${country}`;
 
-    // Durchführung der API-Anfrage mit `fetch`, wobei die oben definierte URL verwendet wird.
     fetch(url)
-        .then(response => response.json()) // Die Antwort der API wird als JSON verarbeitet.
+        .then(response => response.json())
         .then(data => {
-            // Das Element für das Anzeigeergebnis wird abgerufen.
             const resultDiv = document.getElementById('result');
-        
-            // Überprüfung, ob ein Alter in den Daten vorhanden ist, und Anzeige des Alters.
             if (data.age) {
-                // Verwende <span> tags, um das Alter in einer anderen Schriftart und Größe zu formatieren
                 resultDiv.innerHTML = `Dein Opfer ist mit hoher Wahrscheinlichkeit <span class="age">${data.age}</span> Jahre alt.`;
                 let slider = document.getElementById("myRange");
                 slider.classList.remove("hide-thumb");
                 slider.classList.add("show-thumb");
                 slider.value = data.age;
 
+                // Hier wird der Code zur Farbänderung basierend auf dem Alter platziert
+                var ageElement = document.querySelector('.age');
+                var age = parseInt(data.age); // Verwenden Sie das Alter aus der API-Antwort
+                if (age < 40) {
+                    ageElement.style.color = '#ff0000';
+                } else if (age >= 41 && age <= 59) {
+                    ageElement.style.color = '#ffff01';
+                } else {
+                    ageElement.style.color = '#09ff00';
+                }
             } else {
-                // Falls keine Daten gefunden wurden, wird eine entsprechende Nachricht angezeigt.
                 resultDiv.innerHTML = "Keine Daten gefunden. Bitte überprüfen Sie die Eingaben.";
             }
         })
-        
         .catch(error => {
-            // Bei einem Fehler in der Anfrage wird dieser im Konsolen-Log ausgegeben und eine Fehlermeldung angezeigt.
             console.error('Fehler beim Abrufen der Daten: ', error);
             document.getElementById('result').innerHTML = "Fehler bei der Anfrage.";
         });
 }
+
 
 // const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -79,8 +76,6 @@ function fetchAge() {
 // slider.value = 30;
 
 
-
 // const slider = document.getElementById("myRange");
 // const sliderThumb = document.querySelector (".slider: :-webkit-slider-thumb");
-
 
